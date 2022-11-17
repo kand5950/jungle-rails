0271b7016a64d5e1f7f85c627cd3826463e90ebb
Anit <anittankandasamy@gmail.com>
2022-11-17 18:07:55 -0500

    create view file for signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
+  #rendering signup form
   def new
   end
 
+  #receiving form and creating a user with forms parameters
   def create
   end
-  
+
 end
