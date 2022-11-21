8878d2ae639429f8be70d8194bfad1048823b936
Anit <anittankandasamy@gmail.com>
2022-11-21 18:19:06 -0500

    write tests for product model validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     before(:each) do
       @category = Category.create(name: "testCategory")
-      @product = Product.new(
+      @product = Product.create(
         name: "testProduct",
-        price: 10,
+        price_cents: 10,
         quantity: 7,
         category: @category,
       )
@@ -18,6 +18,29 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to be_empty
     end
 
+    it "returns expected error message when no name is provided" do
+      @product.name = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
 
+    it "returns expected error message when no price is provided" do
+      @product.price_cents = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Price cents is not a number", "Price is not a number", "Price can't be blank")
+    end
+
+    it "returns expected error message when no quantity is provided" do
+      @product.quantity = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "returns expected error message when no category is provided" do
+      @product.category = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+    
   end
 end
