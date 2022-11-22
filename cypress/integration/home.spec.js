c3fa27c56667f989568b91b680f038bae99ac7e9
Anit <anittankandasamy@gmail.com>
2022-11-22 15:17:41 -0500

    write test to validate all products shown on home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,4 +6,8 @@ describe('Navigation', () => {
   it('should show products on page', () => {
     cy.get('.products article').should('be.visible');
   });
+
+  it('should show all products on page', () => {
+    cy.get('.products article').should('have.length', 2);
+  });
 });
