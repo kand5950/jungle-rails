331b3a9f71544f15a062a71b48a8b8960ee40ed0
Anit <anittankandasamy@gmail.com>
2022-11-21 22:06:02 -0500

    create new class method in user model taking over logic from session controller, add new method in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    #if the user exists and the password entered is correct
-    if user && user.authenticate(params[:password])
+    
+    #if the user is successfully authenticated
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       #save the user id inside the browser cookie. this is how we keep the user logged in when they navigate around our website
       session[:user_id] = user.id
       redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: 4}
   validates :password_confirmation, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
